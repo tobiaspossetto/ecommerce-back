0ed54db71701dcf8441adf3e08e88baa39b1e2a9
tobiaspossetto <tobigpossetto@gmail.com>
2021-11-11 10:39:09 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+
+import cloudinary from 'cloudinary'
+import IndexRoutes from './routes'
+//initializations
+export const app = express()
+
+
+//settings
+app.set('port', process.env.port || 4000)
+
+// @ts-ignore
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+//middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false})) //poder interpretar datos de un form
+
+
+//routes
+
+app.use('/', IndexRoutes);
+
+
+//starting server
+
+export const server = app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`)
+})
